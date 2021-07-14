c7ff144009673fb49b255f92a5af99fdead0a266
Arsenii <semdestroyer@yandex.ru>
2021-07-14 03:45:54 +0300

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="container">
+      <div class="col-md-6 mx-auto">
+      <div class="card">
+        <div class="card-title">Сервис временной почты NULLMAIL<i class="bi bi-watch"></i>
+        </div>
+        <div class="card-body">
+          <i class="bi bi-envelope-open"><span class="badge rounded-pill bg-danger text-white">1</span></i>
+          <timer></timer>
+          <input type="text"  v-model="email" readonly>
+        </div>
+        <div class="card-footer">
+          <button class="btn btn-primary">Получить новый Email</button>
+        </div>
+      </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+
+import Timer from "./components/Timer";
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {Timer},
+  data(){
+    return{
+      email:""
+    }
+  },
+  methods:{
+    send:function()
+    {
+
+    },
+
+
+  },
+
 }
 </script>
 
---
+++ src/components/Timer.vue
@@ -0,0 +1,34 @@
+<template>
+<h6>{{minutes + ":" + seconds}}</h6>
+</template>
+
+<script>
+export default {
+  name: "Timer",
+  data()
+  {
+    return{
+      minutes:10,
+      seconds:0,
+      allseconds:0,
+    }
+  },
+  methods:{
+    showTime:function() {
+      setInterval(()=>{
+        this.allseconds -= 1;
+        this.minutes = Math.floor(this.allseconds/60);
+        this.seconds = this.allseconds % 60;
+      },1000);
+    }
+  },
+  mounted() {
+    this.allseconds = this.minutes * 60;
+    this.showTime();
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
